3a762babcc737d1ace71d4d8027ef9c3ae4e7380
Marcos Vinicius <contato@marcosviniciusdev.com>
2024-03-15 02:38:02 -0300

    feat: add contrast and brightness function

--- image-processor/processing/transformation.py
+++ image-processor/processing/transformation.py
@@ -1,4 +1,6 @@
 from skimage.transform import resize
+from skimage import exposure
+from skimage import util
 
 
 def resize_image(image, proportion):
@@ -9,3 +11,14 @@ def resize_image(image, proportion):
     image_resized = resize(image, (height, width), anti_aliasing=True)
 
     return image_resized
+
+
+def change_contrast(image, contrast_level):
+    return exposure.adjust_gamma(image, gamma=contrast_level)
+
+
+def change_brightness(image, brightness_level):
+    image_float = util.img_as_float(image)
+    adjusted_image = image_float * brightness_level
+
+    return exposure.adjust_gamma(adjusted_image)
