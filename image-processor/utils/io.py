ca34718acb2c6f51e77cb948d43492c1d0187b04
Marcos Vinicius <contato@marcosviniciusdev.com>
2024-03-15 02:40:25 -0300

    refactor: add type of parameter

--- image-processor/utils/io.py
+++ image-processor/utils/io.py
@@ -1,7 +1,7 @@
 from skimage.io import imread, imsave
 
 
-def read_image(path, is_gray=False):
+def read_image(path: str, is_gray=False):
     image = imread(path, as_grey=is_gray)
     return image
 
