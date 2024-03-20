e60d4505ef53117ed90a90eec9ef47b08d4bdc16
Marcos Vinicius <contato@marcosviniciusdev.com>
2024-03-20 13:12:19 -0300

    refactor: add rescale_image, flip_image_horizontally, flip_image_vertically and mirror_image functions.

--- image-processor/processing/transformation.py
+++ image-processor/processing/transformation.py
@@ -1,16 +1,38 @@
+import numpy
 from skimage.transform import resize
+from skimage.transform import rescale
 from skimage import exposure
 from skimage import util
 
 
-def resize_image(image, proportion):
-    assert 0 <= proportion <= 1, "Specify a valid proportion between 0 and 1."
+def rescale_image(image, proportion):
+    return rescale(image, proportion)
 
-    height = round(image.shape[0] * proportion)
-    width = round(image.shape[1] * proportion)
-    image_resized = resize(image, (height, width), anti_aliasing=True)
 
-    return image_resized
+def resize_image(image, new_size: str):
+    # assert 0 <= proportion <= 1, "Specify a valid proportion between 0 and 1."
+    height, width = map(str.strip, new_size.split(","))
+    height = float(height)
+    width = float(width)
+
+    new_size = tuple(new_size)
+    new_size = (height, width)
+
+    return resize(image, new_size)
+
+
+def flip_image_horizontally(image):
+    return image[:, ::-1, ...]
+
+
+def flip_image_vertically(image):
+    return image[::-1, :, ...]
+
+
+def mirror_image(image):
+    flipped_image = flip_image_horizontally(image)
+
+    return numpy.hstack([image, flipped_image])
 
 
 def change_contrast(image, contrast_level):
